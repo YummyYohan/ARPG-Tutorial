ce3c6838bf6091f110abf505363b4a2ba5196905
YummyYohan <theirstieratol@gmail.com>
2024-07-09 19:14:38 -0400

    Health, knockback, hurt state, enemy collisions

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,21 @@
 extends CharacterBody2D
 
+signal healthChanged
+
 @export var speed: int = 45
 @onready var animations = $AnimationPlayer
+@export var maxHealth = 3
+@onready var currentHealth: int = maxHealth
+@onready var effects = $Effects
+@onready var hurtTimer = $hurtTimer
+
+@export var knockbackPower: int = 500
+
+var isHurt: bool = false
+var enemyCollisions = []
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -18,7 +32,45 @@ func updateAnimation():
 		elif velocity.y < 0: direction = "Up"
 
 		animations.play("walk" + direction)
+
+
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		print_debug(collider.name)
+
 func _physics_process(delta):
+	handleCollision()
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	if !isHurt:
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
+
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+
+func _on_hurt_box_area_entered(area:Area2D):
+	if area.name == "hitBox":
+		enemyCollisions.append(area)
+	
+
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+
+func _on_hurt_box_area_exited(area:Area2D):
+	enemyCollisions.erase(area)
